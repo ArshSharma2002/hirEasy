dd3110c2a4637523978c7df333432673309a5d4d
Parth Sharma <parthsharmabilari295@gmail.com>
2022-05-09 15:56:57 +0530

    navbar changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,7 +25,7 @@ function Navbar(props) {
 
   return (
       <header>
-      <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <nav className="navbar navbar-expand-md navbar-light fixed-top bg-light">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">MaidEasy</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
